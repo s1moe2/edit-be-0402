026f8eafc0a2d5053b2b19c61b7c93d987071aa4
Diogo Simoes <diogo@cloudoki.com>
2024-02-05 20:58:16 +0000

    aula 2 pt2

--- aula2/index.js
+++ aula2/index.js
@@ -1,18 +1,41 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+const posts = [
+  {
+    id: 1,
+    title: "express tutotial",
+    content: "lorem ipsum",
+    date: new Date("2020-04-23"),
+    tags: ["tag1", "tag2"],
+  },
+];
+
 app.use((req, res, next) => {
   console.log("[request]", req.method, req.url);
   next();
   console.log("[response]", req.method, req.url, res.statusCode);
 });
 
-app.post("/tracking", (req, res) => {
-  res.status(200).send("package is on the way");
+app.get("/posts", (req, res) => {
+  res.status(200).json(posts);
 });
 
-app.get("/tracking", (req, res) => {
-  res.status(200).send("package is on the way");
+app.post("/posts", (req, res) => {
+  // validations
+  if (!req.body.title || req.body.title.length < 5) {
+    return res.status(400).json({
+      error: "invalid title",
+    });
+  }
+
+  // add to database
+  posts.push(req.body);
+
+  // response
+  res.status(201).json(posts[posts.length - 1]);
 });
 
 app.listen(3000, () => {
